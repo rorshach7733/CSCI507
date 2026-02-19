9a3fa387497cf95e715ef65c9a0d3453765375ec
rorshach7733 <rorshach7733@gmail.com>
2026-02-19 00:48:53 -0800

    Add files via upload

---
+++ src/matrix.java
@@ -0,0 +1,17 @@
+public class matrix
+
+{public static void main(String[] args) {
+   int[][] A = {
+           {1,2,3},
+           {4,5,6},
+           {7,8,9}
+   };
+
+   System.out.println(A[0][0] + " " + A[0][1] + " " + A[0][2]);
+   System.out.println(A[1][0] + " " + A[1][1] + " " + A[1][2]);
+   System.out.println(A[2][0] + " " + A[2][1] + " " + A[2][2]);
+}
+
+}
+
+// the complexity is n(1)
